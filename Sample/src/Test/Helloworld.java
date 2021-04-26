8835ce2c713977bee2bba8aec4576c2612c33ff6
이성한 <home8392@gmail.com>
2021-04-26 11:45:33 +0900

    Sample - 기본 Console 출력

---
+++ Sample/src/Test/Helloworld.java
@@ -0,0 +1,13 @@
+// 폴더(패키지) 이름
+package Test;
+
+// 파일(.java) 이름
+public class Helloworld {
+	
+	// 내용
+	public static void main(String[] args) {
+		// 이클립스 console에 문구 출력
+		System.out.println("Hello world");
+	}
+
+}

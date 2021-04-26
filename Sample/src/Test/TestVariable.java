1e5ff73362e0725498dd969e3ed394930ff10562
User <User@DESKTOP-JLLS4GI>
2021-04-26 12:15:11 +0900

    변수 자료형 정의

---
+++ Sample/src/Test/TestVariable.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class TestVariable {
+
+	public static void main(String[] args) {
+		// 변수 자료형 정의
+		boolean flag;	// 참, 거짓 true 또는 false
+		char grade;		// 문자 0 ~ 65,535
+		byte value;		// 정수 -128 ~ 128
+		short svalue;		// 정수 -32,768 ~ 32,767
+		int num;			// 정수 -2^31 ~ 2^31-1
+		long price;		// 정수 -2^63 ~ 2^63-1
+		float tax;			// 실수 -3.4E38 ~ +3.4E38
+		double score;	// 실수 -1.7E308 ~ +1.7E308
+	}
+
+}

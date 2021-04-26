1d101ba3c71723a58036ac36f4c48fd8f1259f22
SunghanLee <home8392@gmail.com>
2021-04-27 08:41:23 +0900

    증감 연산자

---
+++ Sample/src/Test/TestSign.java
@@ -0,0 +1,30 @@
+package Test;
+
+public class TestSign {
+
+	public static void main(String[] args) {
+		int value = -57;
+		
+		int result1 = +value;
+		int result2 = -value;
+		
+		System.out.println("value : " + value);
+		System.out.println("result1 : " + result1);
+		System.out.println("result2 : " + result2);
+		
+		int x = 12;
+		int y = 12;
+		
+		// ++가 앞에 붙으면 값을 1 증가 시킨 후 대입
+		System.out.println("x: " + x);
+		int result3 = ++x ;
+		System.out.println("x: " + x + ", result3 = " + result3);
+		
+		// ++가 뒤에 붙으면 값을 대입 시킨 후 1 증가
+		System.out.println("y: " + y);
+		int result4 = y++;
+		System.out.println("y: " + y + ", result4 = " + result4);
+				
+	}
+
+}

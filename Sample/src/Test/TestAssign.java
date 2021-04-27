0da519376dce5f797edf42e35764664efe7dcaf5
SunghanLee <home8392@gmail.com>
2021-04-27 15:13:16 +0900

    대입 연산자, 삼항 연산자

---
+++ Sample/src/Test/TestAssign.java
@@ -0,0 +1,30 @@
+package Test;
+
+public class TestAssign {
+	
+	public static void main(String[] args){
+		int i = 1;
+		
+		// 대입 연산자
+		i += 2;
+		System.out.println("i += 2 -> i = " + i);
+		
+		i -= 2;
+		System.out.println("i = 3, i -= 2 -> i = " + i);
+		
+		i *= 2;
+		System.out.println("i = 1, i *= 2 -> i = " + i);
+		
+		i /= 2;
+		System.out.println("i = 2, i /= 2 -> i = " + i);
+		
+		// % 는 뒤 숫자로 나누고 난 나머지를 뜻한다.
+		i %= 2;
+		System.out.println("i = 1, i %= 2 -> i = " + i);
+		
+		// 삼항 연산자
+		int value = 123;
+		String result = (value % 2 == 0)? "짝수" : "홀수";
+		System.out.println(value + "는 " + result + "이다.");
+	}
+}

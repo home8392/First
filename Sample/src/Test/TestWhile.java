ea7e8cc62bf447f78f072594c4743aec96c7fec8
SunghanLee <home8392@gmail.com>
2021-04-28 20:15:41 +0900

    While 문

---
+++ Sample/src/Test/TestWhile.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class TestWhile {
+
+	public static void main(String[] args) {
+		int i = 1;
+		int sum = 0;
+		
+		while(i <= 100) {	// while(조건식) 이 참(true)일 경우 실행되는 문장들
+			sum += i;
+			i++;
+		}
+		System.out.println("1부터 100까지 정수들의 합 : " + sum);
+	}
+
+}

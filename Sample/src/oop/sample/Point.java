58f6a6c3abf1917fe095239709b0fb9a30cf6b66
SunghanLee <home8392@gmail.com>
2021-05-12 10:46:22 +0900

    x, y 좌표를 이용해 점을 표현하는 Point 클래스

---
+++ Sample/src/oop/sample/Point.java
@@ -0,0 +1,27 @@
+package oop.sample;
+
+public class Point {		// 클래스 헤더
+	private int posX;	// 멤버필드
+	private int posY;
+	
+	// 메소드
+	public int getPosX() {
+		return posX;
+	}
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+	public int getPosY() {
+		return posY;
+	}
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	
+	// 생성자
+	public Point() {}
+	public Point(int posX, int posY) {
+		this.posX = posX;
+		this.posY = posY;		
+	}
+}
